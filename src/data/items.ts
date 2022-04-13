03dfc9ddf06c8c903cdd1e6e89f058c0552d1692
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-12 22:50:11 -0300

    creates typed metadata

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 5, 12),
+    category: "food",
+    title: "mcdonalds",
+    value: 33.12,
+  },
+  {
+    date: new Date(2022, 5, 12),
+    category: "food",
+    title: "Burger King",
+    value: 40.12,
+  },
+  {
+    date: new Date(2022, 5, 12),
+    category: "rent",
+    title: "Appartment Rent",
+    value: 1200,
+  },
+  {
+    date: new Date(2022, 6, 18),
+    category: "salary",
+    title: "Salary",
+    value: 4500,
+  },
+];
