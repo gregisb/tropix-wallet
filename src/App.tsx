63c6891a4128e13705ead5f115ee9594f518003b
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-18 20:49:45 -0300

    display filtered income and expense data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import { useState, useEffect } from 'react';
-import * as C from './App.styles';
-import { Item } from './types/Item';
-import { categories } from './data/categories';
-import { items } from './data/items';
-import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
-import { TableArea } from './components/TableArea';
-import { InfoArea } from './components/InfoArea';
+import { useState, useEffect } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
+import { TableArea } from "./components/TableArea";
+import { InfoArea } from "./components/InfoArea";
 
 const App = () => {
-
-    const [list, setList] = useState(items);
+  const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth()); //atualiza o estado com o mês atual
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
-  useEffect(()=> {
+  useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
 
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList])
+
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
-  };  
+  };
 
   return (
     <C.Container>
@@ -28,10 +44,12 @@ const App = () => {
       </C.Header>
 
       <C.Body>
-        <InfoArea 
-              currentMonth={currentMonth}
-              onMonthChange={handleMonthChange}
-              />
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
 
         {/* insert area */}
 
