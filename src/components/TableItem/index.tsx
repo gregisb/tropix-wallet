38ba7f96f110dc12c8ad3e8ed69d605d809349b6
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-18 12:18:42 -0300

    creates formatDate function

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,5 +1,6 @@
 import * as C from './styles';
-import { Item } from '../../types/Item'
+import { Item } from '../../types/Item';
+import { formatDate } from '../../helpers/dateFilter'
 
 type Props = {
     item: Item
@@ -8,7 +9,7 @@ type Props = {
 export const TableItem = ({item}: Props) => {
     return (
         <C.TableLine>
-            <C.TableCollumn>Data</C.TableCollumn>
+            <C.TableCollumn>{formatDate(item.date)}</C.TableCollumn>
             <C.TableCollumn>{item.category}</C.TableCollumn>
             <C.TableCollumn>{item.title}</C.TableCollumn>
             <C.TableCollumn>$ {item.value}</C.TableCollumn>
